59def28a35652a66c9525bf75cd57686df2f9d99
JunaidKhanEU <junaidkhan1@gmx.com>
2020-03-09 16:55:01 +0000

    banner section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import { Fa500Px } from 'react-icons/fa'
-const IndexPage = () => (
-  <Layout>
-    <Seo title='Home' keywords={['gatsby', 'application', 'react']} />
-    <h2>SOmething</h2>
-    <Fa500Px />
-  </Layout>
-)
+import BackgroundSection from '../components/Globals/BackgroundSection/BackgroundSection'
 
+const IndexPage = ({ data }) => {
+  const { image: { childImageSharp: { fluid } } } = data
+  console.log(fluid)
+  return (
+    <Layout>
+      <Seo title='Home' keywords={['gatsby', 'application', 'react']} />
+      <BackgroundSection img={fluid} title='Regular Joe`s Coffee' styleClass='default-background' />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+query{
+  image:file(relativePath:{eq:"about-background.jpeg"}){
+    childImageSharp{
+      fluid{
+       ...GatsbyImageSharpFluid_withWebp
+      }
+    }
+  }
+}
+`
 export default IndexPage
