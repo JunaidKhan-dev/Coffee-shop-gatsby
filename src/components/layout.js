eabad7b1b76135da51fd92d4714f0cf9a095ab60
JunaidKhanEU <junaidkhan1@gmx.com>
2020-03-09 12:17:37 +0000

    navbar added

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './bootstrap.min.css'
 import './layout.css'
+import Navbar from './Globals/Navbar/Navbar'
 
 const Layout = ({ children }) => {
   return (
     <>
-      <header className='bg-info'>header</header>
+      <Navbar />
       {children}
     </>
   )
