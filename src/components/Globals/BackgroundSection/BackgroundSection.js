b6b6e96d52da49ef3183079926d13bada36bc840
JunaidKhanEU <junaidkhan1@gmx.com>
2020-03-10 09:51:44 +0000

    linear gredient fix opacity:1!important;

--- src/components/Globals/BackgroundSection/BackgroundSection.js
+++ src/components/Globals/BackgroundSection/BackgroundSection.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import BackgroundImage from 'gatsby-background-image'
-const BackgroundSection = ({ img, styleClass, title, children }) => {
+import styled from 'styled-components'
+const BackgroundSection = ({ img, className, title, children }) => {
   return (
-    <BackgroundImage fluid={img} className={styleClass}>
+    <BackgroundImage fluid={img} className={className}>
       <h1 className='title text-white text-uppercase text-center display-4 font-weight-bold'>{title}</h1>
       {children}
     </BackgroundImage>
@@ -10,7 +11,16 @@ const BackgroundSection = ({ img, styleClass, title, children }) => {
 }
 
 BackgroundSection.defaultProps = {
-  title: 'default title',
-  styleClass: 'default-background'
+  title: 'default title'
 }
-export default BackgroundSection
+export default styled(BackgroundSection)`
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(rgba(255, 158, 9, 0.5), rgba(255, 158, 9, 0.5));
+  background-position: center;
+  background-size: cover;
+  /* this is very important to put opacity equal to 1 with !important ketword so that linear gredient can work */
+  opacity: 1 !important;  
+`
